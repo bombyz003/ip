d04fcfc890cf2df18e3ba0f4cf757ffdf2a1aa5a
Yi zhong <icyblast310@gmail.com>
2026-01-24 02:04:36 +0800

    1st commit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# TalkCok User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/TalkCok.java
@@ -0,0 +1,7 @@
+public class TalkCok {
+    public static void main(String[] args) {
+        System.out.println("____________________________");
+        System.out.println("Hi, I'm TalkCok!\nWhat can I do for you?\n");
+        System.out.println("Bye, see you again soon!");
+    }
+}
