aba36e68b6cfc17bc1116ef8f19112e46316b072
Yi zhong <icyblast310@gmail.com>
2026-01-24 18:22:04 +0800

    Level 1

--- src/main/java/TalkCok.java
+++ src/main/java/TalkCok.java
@@ -1,7 +1,31 @@
+import java.util.*;
+
 public class TalkCok {
     public static void main(String[] args) {
+        int c = 0;
+        Scanner scan = new Scanner(System.in);
+        String input;
+
         System.out.println("____________________________");
-        System.out.println("Hi, I'm TalkCok!\nWhat can I do for you?\n");
-        System.out.println("Bye, see you again soon!");
+        System.out.println("Hi, I'm TalkCok!");
+        System.out.print("Not asking what can I do because in here I'm just gonna echo what you say.\n");
+        do {
+            input = scan.nextLine();
+            c++;
+
+            if (input.length() > 200) {
+                System.out.println("Your input is too long bitch. Try again lol.\n");
+                continue;
+            }
+            if (!input.equalsIgnoreCase("bye")) {
+                System.out.println(input);
+            }
+            if(c % 3 == 0) {
+                System.out.println("(Enter bye to quit)");
+            }
+        } while (!input.equalsIgnoreCase("bye"));
+
+        System.out.println("Bye, awesome talk!!");
+        scan.close();
     }
 }
