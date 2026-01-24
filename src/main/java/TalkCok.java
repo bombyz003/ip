c26763f61523ec15a8400e23df30011884aa224e
Yi zhong <icyblast310@gmail.com>
2026-01-25 00:05:42 +0800

    Level 2

--- src/main/java/TalkCok.java
+++ src/main/java/TalkCok.java
@@ -2,30 +2,35 @@ import java.util.*;
 
 public class TalkCok {
     public static void main(String[] args) {
-        int c = 0;
         Scanner scan = new Scanner(System.in);
         String input;
+        List<String> ls = new ArrayList<>();
 
         System.out.println("____________________________");
         System.out.println("Hi, I'm TalkCok!");
-        System.out.print("Not asking what can I do because in here I'm just gonna echo what you say.\n");
+        System.out.println("What do ya have for me today? (enter bye to quit)\n");
         do {
             input = scan.nextLine();
-            c++;
 
-            if (input.length() > 200) {
-                System.out.println("Your input is too long bitch. Try again lol.\n");
+            int c = 1;
+            if (input.equalsIgnoreCase("list")) {
+                System.out.println("_____________________________");
+                for (String listItem: ls) {
+                    System.out.println(c + ". " + listItem);
+                    c++;
+                }
                 continue;
             }
-            if (!input.equalsIgnoreCase("bye")) {
-                System.out.println(input);
+
+            if (input.equalsIgnoreCase("bye")) {
+                break;
             }
-            if(c % 3 == 0) {
-                System.out.println("(Enter bye to quit)");
-            }
+            ls.add(input);
+            System.out.println("added: " + input);
+
         } while (!input.equalsIgnoreCase("bye"));
 
-        System.out.println("Bye, awesome talk!!");
+        System.out.println("Aw man, bye bye!");
         scan.close();
     }
 }
